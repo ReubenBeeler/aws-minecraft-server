0efb435812bd50744e2e2cf08519d922681d499e
ReubenBeeler <reubenfbeeler@gmail.com>
2025-12-12 00:37:22 -0700

    mount instancestore in StartPre

--- bin/StartPre.sh
+++ bin/StartPre.sh
@@ -3,6 +3,14 @@
 PACKAGE_DIR="/opt/aws-minecraft-server"
 FIFO_PATH="$PACKAGE_DIR/fifo"
 MOUNT_POINT="/mnt/instancestore"
+NVME_PARTITION="/dev/nvme1n1"
+
+if ! mount | grep -q "$NVME_PARTITION on $MOUNT_POINT"; then
+    sudo mkfs.ext4 -F $NVME_PARTITION
+    sudo mkdir -p $MOUNT_POINT
+    sudo mount $NVME_PARTITION $MOUNT_POINT
+    sudo chmod 1777 $MOUNT_POINT
+fi
 
 if [[ ! -p $FIFO_PATH ]]; then
     sudo rm -f $FIFO_PATH
