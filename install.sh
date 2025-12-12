53426901c29770713ce5daced1b9343f247a8d94
ReubenBeeler <reubenfbeeler@gmail.com>
2025-12-12 02:04:11 -0700

    more perms

--- install.sh
+++ install.sh
@@ -21,8 +21,9 @@ sudo mv $prev_dir $PACKAGE_DIR
 sudo ln -s $PACKAGE_DIR $prev_dir
 unset prev_dir
 
-find $PACKAGE_DIR -type d -exec chmod 774 {} \;
-find $PACKAGE_DIR -type f -exec chmod 660 {} \;
+find $PACKAGE_DIR -exec chown ec2-user:minecraft {} \;
+find $PACKAGE_DIR -exec chmod 660 {} \;
+find $PACKAGE_DIR -type d -exec chmod 776 {} \;
 find $PACKAGE_DIR/bin -exec chmod 760 {} \;
 sudo ln -f $PACKAGE_DIR/minecraft.service /etc/systemd/system/minecraft.service
 # Make group and add user to group for minecraft.service
