e7046145ed4678088d8a5efeffe47b2b16e6dcf2
ReubenBeeler <reubenfbeeler@gmail.com>
2025-12-14 00:38:12 -0700

    MO DEDITATED WAM

--- bin/Start.sh
+++ bin/Start.sh
@@ -4,4 +4,4 @@ PACKAGE_DIR="/opt/aws-minecraft-server"
 MOUNT_POINT="/mnt/instancestore"
 
 cd $MOUNT_POINT/minecraft
-tail -f $PACKAGE_DIR/fifo | /usr/bin/java -Xms4G -Xmx6G -jar server.jar nogui
+tail -f $PACKAGE_DIR/fifo | /usr/bin/java -Xms6500M -Xmx6500M -jar server.jar nogui
